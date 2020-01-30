4312ecc6e69d5da9713497c2dc54fa5c1c1b8fb8
Marcus <mcrowley2@mail.usf.edu>
2020-01-30 18:32:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserInputFirstNameLastName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a variable
+            string name;
+            
+            // Introduce yourself
+            Console.WriteLine("Hi, this is Marcus Crowley!");
+
+            // Ask user to type user's name
+            Console.Write("What is your name?: ");
+
+            // Store whatever is typed into the user's input variable
+            name = Console.ReadLine();
+
+            // Print blank line
+            Console.WriteLine();
+
+            // Show what the user entered
+            Console.WriteLine("Nice to meet you {0}!", name);
+        }
+    }
+}
